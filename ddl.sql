064011512750073afb72c304770ed535e5d103da
Wesley Tsang <tsangwe@oocl.com>
2019-12-06 17:23:54 +0800

    completed

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,46 @@
 -- **Database level:**
 -- Display all the database
+SHOW DATABASES;
 
 -- Enter a certain database
+USE dbname;
 
 -- Create a database
+CREATE DATABASE dbname;
 
 -- Create the database of the designated character set
+create database dbname character set UTF8mb4 collate utf8mb4_bin;
 
 -- Display the creation information fo the database
+SHOW CREATE DATABASE dbname;
 
 -- Revise the codes of the database
+ALTER DATABASE dbname UPGRADE DATA DIRECTORY NAME;
 
 -- Delete a database
+DROP DATABASE daname;
 
 -- **Table level**
 -- Revise table name
+SHOW TABLES;
 
 -- Revise the field's data type
+ALTER TABLE s.t MODIFY COLUMN id int(11) NULL;
 
 -- Revise field name
+ALTER TABLE s.t CHANGE id ind varchar(10) NULL;
 
 -- Add field
+ALTER TABLE s.t ADD column1 varchar(100) NULL;
 
 -- Delete field
+ALTER TABLE s.t DROP COLUMN create_iodt;
 
 -- Revise the table's storage engine
+ALTER TABLE s.t ENGINE=CSV;
 
 -- Delete the table's foreign key restriant
+ALTER TABLE s.t DROP FOREIGN KEY `id_name_fk`;
 
 -- Delete a table
+DROP TABLE s.t;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
 -- Insert record
+INSERT INTO table_name VALUES (value1, value2, value3);
 
 -- Revise record
+UPDATE table_name SET column1 = value1, column2 = value2 WHERE id = 3;
 
 --  Delete record
+DELETE FROM table_name WHERE id = 3;
 
 -- Search record
+SELECT * FROM table_name WHERE id = 3;
